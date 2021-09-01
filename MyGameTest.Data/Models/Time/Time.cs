cf7f16cb12274b0ae3058a268a3c22bda873d31a
Александр Мильчаков <LaikWQC@gmail.com>
2021-09-01 18:55:28 +0300

    new 60fps timer

--- MyGameTest.Data/Models/Time/Time.cs
+++ MyGameTest.Data/Models/Time/Time.cs
@@ -4,42 +4,43 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Timers;
 
 namespace MyGameTest.Models
 {
     public class Time : ITime
     {
-        private Timer _timer;
-        private int _ticksPerSec = 50;
-        private double _baseDeltaTime;
-        private bool _isUpdating = false;
-        private DateTime _lastUpdate;
+        private Task _loopTask;
+        private int _fps;
 
-        public Time()
+        public Time(int fps = 60)
         {
-            _lastUpdate = DateTime.Now;
-            _baseDeltaTime = 1d / _ticksPerSec;
-
-            _timer = new Timer(_baseDeltaTime * 1000);
-            _timer.AutoReset = true;
-            _timer.Elapsed += OnElapsed;
+            _fps = Math.Min(Math.Max(fps, 1), 500);
         }
 
-        private void OnElapsed(object sender, ElapsedEventArgs e)
+        public void Loop()
         {
-            if (_isUpdating) return;
+            var lastUpdateTime = DateTime.UtcNow;
+            var baseDeltaTime = 1d / _fps;
+            //TODO add cancelation token
+            while (true)
+            {
+                var timeNow = DateTime.UtcNow;
+                var deltaTime = (timeNow - lastUpdateTime).TotalSeconds;
+                lastUpdateTime = timeNow;
+                OnUpdate?.Invoke(deltaTime);
 
-            _isUpdating = true;
-
-            OnUpdate?.Invoke((e.SignalTime - _lastUpdate).TotalSeconds);
-            _lastUpdate = e.SignalTime;
-
-            _isUpdating = false;
+                var nextUpdateTime = timeNow.AddSeconds(baseDeltaTime).AddMilliseconds(-1);
+                while (DateTime.UtcNow < nextUpdateTime)
+                    System.Threading.Thread.Sleep(1);
+            }
         }
 
         public event Action<double> OnUpdate;
 
-        public void Start() => _timer.Start();
+        public void Start()
+        {
+            _loopTask = new Task(Loop);
+            _loopTask.Start();
+        }
     }
 }
---
+++ MyGameTest.Data/Models/Time/Time2.cs
@@ -0,0 +1,42 @@
+﻿using MyGameTest.Services;
+using System;
+using System.Timers;
+
+namespace MyGameTest.Models
+{
+    [Obsolete]
+    public class Time2 : ITime
+    {
+        private Timer _timer;
+        private int _ticksPerSec = 50;
+        private double _baseDeltaTime;
+        private bool _isUpdating = false;
+        private DateTime _lastUpdate;
+
+        public Time2()
+        {
+            _lastUpdate = DateTime.Now;
+            _baseDeltaTime = 1d / _ticksPerSec;
+
+            _timer = new Timer(_baseDeltaTime * 1000);
+            _timer.AutoReset = true;
+            _timer.Elapsed += OnElapsed;
+        }
+
+        private void OnElapsed(object sender, ElapsedEventArgs e)
+        {
+            if (_isUpdating) return;
+
+            _isUpdating = true;
+
+            OnUpdate?.Invoke((e.SignalTime - _lastUpdate).TotalSeconds);
+            _lastUpdate = e.SignalTime;
+
+            _isUpdating = false;
+        }
+
+        public event Action<double> OnUpdate;
+
+        public void Start() => _timer.Start();
+    }
+}
